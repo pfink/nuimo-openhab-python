f60e1d3316726eb59d8d1e8b2508d2d74a7fd9fc
Patrick Fink <mail@pfink.de>
2017-05-27 03:05:18 +0200

    Add automatic discovery of chosen Nuimo

--- main.py
+++ main.py
@@ -6,13 +6,24 @@ openhab = openHAB(config["openhab_api_url"])
 apps = OpenHabAppBuilder().buildApps(openhab)
 
 manager = nuimo.ControllerManager(adapter_name='hci0')
-controller = nuimo.Controller(mac_address=config["nuimo_mac_address"], manager=manager)
-menue = NuimoMenue(apps=apps, controller=controller)
-# Register listener
-controller.listener = NuimoMenueControllerListener(menue)
+
+class ControllerManagerDiscoveryListener(nuimo.ControllerManagerListener):
+    def controller_discovered(self, controller):
+        print("Discovered Nuimo controller:", controller.mac_address)
+        if(controller.mac_address == str(config["nuimo_mac_address"]).lower()):
+            print("Found configured Nuimo with MAC address "+config["nuimo_mac_address"])
+            # Initialize App
+            menue = NuimoMenue(apps=apps, controller=controller)
+            controller.listener = NuimoMenueControllerListener(menue)
+            controller.connect()
+            menue.showIcon()
+        else:
+            print("Discovered Nuimo "+controller.mac_address+" does not match with configured "+config["nuimo_mac_address"]+". Continue discovery...")
+
+
+manager.listener = ControllerManagerDiscoveryListener()
 
 while(True):
-    controller.connect()
-    menue.showIcon()
+    manager.start_discovery()
     manager.run()
     time.sleep(5)
